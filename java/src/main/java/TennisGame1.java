1961ac7b703f9e1f9bb42914e631c79b161486a4
adrg2 <adr.garro@gmail.com>
2022-10-21 19:05:23 +0200

    refactor: introduce var

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -28,7 +28,7 @@ public class TennisGame1 implements TennisGame {
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            score = winOrAdvantage();
+            score = winOrAdvantage(m_score1, m_score2);
         }
         else
         {
@@ -57,9 +57,9 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
-    private String winOrAdvantage() {
+    private String winOrAdvantage(int matchScore1, int matchScore2) {
         String score;
-        int minusResult = m_score1-m_score2;
+        int minusResult = matchScore1 - matchScore2;
         if (minusResult==1) score ="Advantage player1";
         else if (minusResult ==-1) score ="Advantage player2";
         else if (minusResult>=2) score = "Win for player1";
