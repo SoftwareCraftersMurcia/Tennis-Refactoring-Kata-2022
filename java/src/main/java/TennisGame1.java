26eb28bfea396c299fceb5b3375bd2f0f0027597
tmorenobernal <tmorenobernal@gmail.com>
2022-10-21 19:23:40 +0200

    Borrar variables que no se están usando

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,13 +3,9 @@ public class TennisGame1 implements TennisGame {
     
     private int pointsPlayer1;
     private int pointsPlayer2;
-    private String player1Name;
-    private String player2Name;
 
+    @Deprecated
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-
         pointsPlayer1 = 0;
         pointsPlayer2 = 0;
     }
