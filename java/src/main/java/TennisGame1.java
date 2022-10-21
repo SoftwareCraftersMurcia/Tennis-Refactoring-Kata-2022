02bac21e65ee0211a35067bc61bb8b97224f7d38
adrg2 <adr.garro@gmail.com>
2022-10-21 19:07:10 +0200

    refactor: removed score var

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -58,13 +58,11 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String winOrAdvantage(int matchScore1, int matchScore2) {
-        String score;
         int minusResult = matchScore1 - matchScore2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-        return score;
+        if (minusResult==1) return "Advantage player1";
+        else if (minusResult ==-1) return "Advantage player2";
+        else if (minusResult>=2) return "Win for player1";
+        else return "Win for player2";
     }
 
     private String drawScoreCalculate(int score) {
